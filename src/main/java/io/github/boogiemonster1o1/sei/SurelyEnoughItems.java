8de881358ca3f631ae12ee1ee9275fe76be258ec
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-04 22:49:30 +0530

    Mod-ified files
     Changes to be committed:
    	modified:   .gitignore
    	modified:   gradle.properties
    	new file:   src/main/java/io/github/boogiemonster1o1/sei/SurelyEnoughItems.java
    	deleted:    src/main/java/net/fabricmc/example/ExampleMod.java
    	deleted:    src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
    	modified:   src/main/resources/fabric.mod.json
    	modified:   src/main/resources/modid.mixins.json

---
+++ src/main/java/io/github/boogiemonster1o1/sei/SurelyEnoughItems.java
@@ -0,0 +1,15 @@
+package io.github.boogiemonster1o1.sei;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SurelyEnoughItems implements ModInitializer {
+
+    static final Logger LOGGER = LogManager.getLogger(SurelyEnoughItems.class);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[{}] Initializing","SurelyEnoughItems");
+    }
+}
